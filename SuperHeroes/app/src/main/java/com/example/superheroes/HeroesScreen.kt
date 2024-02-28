01bf02cef5a69ab6b5a4e2e3b93bfcdb813b2d98
yw.hao <yw.hao@trip.com>
2024-02-28 21:07:36 +0800

    feat: add compose example

---
+++ SuperHeroes/app/src/main/java/com/example/superheroes/HeroesScreen.kt
@@ -0,0 +1,9 @@
+package com.example.superheroes
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HeroesScreen(modifier: Modifier = Modifier) {
+
+}
