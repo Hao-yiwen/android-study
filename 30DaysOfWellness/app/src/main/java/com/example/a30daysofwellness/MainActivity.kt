f3b29c0404d9c1f7f7867bdeffe443bfcaeed8c2
yw.hao <yw.hao@trip.com>
2024-03-01 23:49:02 +0800

    feat: completer ui

--- 30DaysOfWellness/app/src/main/java/com/example/a30daysofwellness/MainActivity.kt
+++ 30DaysOfWellness/app/src/main/java/com/example/a30daysofwellness/MainActivity.kt
@@ -3,21 +3,42 @@ package com.example.a30daysofwellness
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.a30daysofwellness.model.Day
 import com.example.a30daysofwellness.model.DaysData
 import com.example.a30daysofwellness.ui.theme._30DaysOfWellnessTheme
 
@@ -42,11 +63,48 @@ class MainActivity : ComponentActivity() {
 fun DaysApp(name: String, modifier: Modifier = Modifier) {
     Scaffold(topBar = {
         DaysTopBar(Modifier)
-    }, modifier = modifier) {
+    }) {
         LazyColumn(contentPadding = it) {
             items(DaysData.list) {
-                Text(text = it.title)
+                DaysItem(it)
             }
+        }
+    }
+}
+
+@Composable
+fun DaysItem(content: Day, modifier: Modifier = Modifier) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ) {
+        Column(modifier = modifier.padding(8.dp)) {
+            Text(
+                text = "Day" + content.day.toString(),
+                style = MaterialTheme.typography.displayMedium,
+                color = MaterialTheme.colorScheme.tertiary
+            )
+            Text(
+                text = content.title,
+                style = MaterialTheme.typography.bodyLarge,
+                modifier = Modifier.padding(top = 16.dp)
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Image(
+                painter = painterResource(id = content.image),
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp)
+                    .clip(MaterialTheme.shapes.medium),
+                contentScale = ContentScale.Crop
+            )
+            Text(
+                text = content.description,
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier.padding(top = 18.dp)
+            )
         }
     }
 }
@@ -54,14 +112,17 @@ fun DaysApp(name: String, modifier: Modifier = Modifier) {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DaysTopBar(modifier: Modifier) {
-    CenterAlignedTopAppBar(title = {
-        Row(verticalAlignment = Alignment.CenterVertically) {
+    CenterAlignedTopAppBar(
+        title = {
             Text(
                 text = stringResource(id = R.string.app_name),
                 style = MaterialTheme.typography.displayLarge
             )
-        }
-    }, modifier = modifier)
+        },
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+        )
+    )
 }
 
 @Preview(showBackground = true)
--- 30DaysOfWellness/app/src/main/java/com/example/a30daysofwellness/ui/theme/Type.kt
+++ 30DaysOfWellness/app/src/main/java/com/example/a30daysofwellness/ui/theme/Type.kt
@@ -2,33 +2,48 @@ package com.example.a30daysofwellness.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.a30daysofwellness.R
 
-// Set of Material typography styles to start with
+val SrirachaFont = FontFamily(Font(R.font.sriracha_regular))
+
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = SrirachaFont,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = SrirachaFont,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.25.sp,
+    ),
+    bodySmall = TextStyle(
+        fontFamily = SrirachaFont,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.4.sp
+    ),
+    displayLarge = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
+        fontSize = 34.sp,
+        lineHeight = 40.sp,
         letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+    displayMedium = TextStyle(
+        fontFamily = SrirachaFont,
+        fontWeight = FontWeight.Normal,
+        fontSize = 20.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
     )
-    */
 )
