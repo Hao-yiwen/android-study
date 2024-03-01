ec9e4ad383954c03a9bc5f0a792fb9cfec930d58
yw.hao <yw.hao@trip.com>
2024-03-01 19:49:12 +0800

    feat: add 30 days of wellness app

---
+++ 30DaysOfWellness/app/src/main/java/com/example/a30daysofwellness/model/DaysData.kt
@@ -0,0 +1,188 @@
+package com.example.a30daysofwellness.model
+
+import com.example.a30daysofwellness.R
+
+object DaysData {
+    val list = listOf<Day>(
+        Day(
+            "Day 1: Walk 10,000 steps",
+            1,
+            R.drawable.day1,
+            "Walk 10,000 steps"
+        ),
+        Day(
+            "Day 2: Drink 8 glasses of water",
+            2,
+            R.drawable.day2,
+            "Drink 8 glasses of water"
+        ),
+        Day(
+            "Day 3: Eat 5 servings of fruits and vegetables",
+            3,
+            R.drawable.day3,
+            "Eat 5 servings of fruits and vegetables"
+        ),
+        Day(
+            "Day 4: Get 8 hours of sleep",
+            4,
+            R.drawable.day4,
+            "Get 8 hours of sleep"
+        ),
+        Day(
+            "Day 5: Meditate for 10 minutes",
+            5,
+            R.drawable.day5,
+            "Meditate for 10 minutes"
+        ),
+        Day(
+            "Day 6: No added sugar",
+            6,
+            R.drawable.day1,
+            "No added sugar"
+        ),
+        Day(
+            "Day 7: No processed foods",
+            7,
+            R.drawable.day2,
+            "No processed foods"
+        ),
+        Day(
+            "Day 8: No caffeine",
+            8,
+            R.drawable.day3,
+            "No caffeine"
+        ),
+        Day(
+            "Day 9: No alcohol",
+            9,
+            R.drawable.day4,
+            "No alcohol"
+        ),
+        Day(
+            "Day 10: No meat",
+            10,
+            R.drawable.day5,
+            "No meat"
+        ),
+        Day(
+            "Day 11: No dairy",
+            11,
+            R.drawable.day1,
+            "No dairy"
+        ),
+        Day(
+            "Day 12: No gluten",
+            12,
+            R.drawable.day2,
+            "No gluten"
+        ),
+        Day(
+            "Day 13: No fast food",
+            13,
+            R.drawable.day3,
+            "No fast food"
+        ),
+        Day(
+            "Day 14: No fried food",
+            14,
+            R.drawable.day4,
+            "No fried food"
+        ),
+        Day(
+            "Day 15: No soda",
+            15,
+            R.drawable.day5,
+            "No soda"
+        ),
+        Day(
+            "Day 16: No snacking",
+            16,
+            R.drawable.day1,
+            "No snacking"
+        ),
+        Day(
+            "Day 17: No eating out",
+            17,
+            R.drawable.day2,
+            "No eating out"
+        ),
+        Day(
+            "Day 18: No eating after 7pm",
+            18,
+            R.drawable.day3,
+            "No eating after 7pm"
+        ),
+        Day(
+            "Day 19: No eating in front of the TV",
+            19,
+            R.drawable.day4,
+            "No eating in front of the TV"
+        ),
+        Day(
+            "Day 20: No eating in the car",
+            20,
+            R.drawable.day5,
+            "No eating in the car"
+        ),
+        Day(
+            "Day 21: No eating in bed",
+            21,
+            R.drawable.day1,
+            "No eating in bed"
+        ),
+        Day(
+            "Day 22: No eating at your desk",
+            22,
+            R.drawable.day2,
+            "No eating at your desk"
+        ),
+        Day(
+            "Day 23: No eating while cooking",
+            23,
+            R.drawable.day3,
+            "No eating while cooking"
+        ),
+        Day(
+            "Day 24: No eating while standing",
+            24,
+            R.drawable.day4,
+            "No eating while standing"
+        ),
+        Day(
+            "Day 25: No eating while walking",
+            25,
+            R.drawable.day5,
+            "No eating while walking"
+        ),
+        Day(
+            "Day 26: No eating while driving",
+            26,
+            R.drawable.day1,
+            "No eating while driving"
+        ),
+        Day(
+            "Day 27: No eating while on the phone",
+            27,
+            R.drawable.day2,
+            "No eating while on the phone"
+        ),
+        Day(
+            "Day 28: No eating while working",
+            28,
+            R.drawable.day3,
+            "No eating while working"
+        ),
+        Day(
+            "Day 29: No eating while reading",
+            29,
+            R.drawable.day4,
+            "No eating while reading"
+        ),
+        Day(
+            "Day 30: No eating while watching TV",
+            30,
+            R.drawable.day5,
+            "No eating while watching TV"
+        )
+    )
+}
