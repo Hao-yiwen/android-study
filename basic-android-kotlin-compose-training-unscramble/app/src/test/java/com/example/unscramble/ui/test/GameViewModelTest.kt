afe61797c1b4a122f23f252fd23e68432043bd58
yw.hao <yw.hao@trip.com>
2024-03-05 21:09:54 +0800

    feat: add TODO md

---
+++ basic-android-kotlin-compose-training-unscramble/app/src/test/java/com/example/unscramble/ui/test/GameViewModelTest.kt
@@ -0,0 +1,33 @@
+package com.example.unscramble.ui.test
+
+import com.example.unscramble.data.SCORE_INCREASE
+import com.example.unscramble.data.getUnscrambledWord
+import com.example.unscramble.ui.GameViewModel
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Test
+
+class GameViewModelTest {
+    private val viewModel = GameViewModel()
+
+    @Test
+    fun gameViewModel_CorrectWordGuessed_ScoreUpdatedAndErrorFlagUnset() {
+        var currentGameUiState = viewModel.uiState.value
+        val unScrambledWord = getUnscrambledWord("at")
+        viewModel.updateUserGuess(unScrambledWord)
+        viewModel.checkUserGuess()
+
+        currentGameUiState = viewModel.uiState.value
+        assertFalse(currentGameUiState.isGuessedWordWrong)
+        assertEquals(SCORE_AFTER_FIRST_CORRECT_ANSWER, currentGameUiState.score)
+    }
+
+    @Test
+    fun gameViewModel_IncorrectGuess_ErrorFlagSet() {
+
+    }
+
+    companion object {
+        private const val SCORE_AFTER_FIRST_CORRECT_ANSWER = SCORE_INCREASE
+    }
+}
