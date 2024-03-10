00599c52ebc1d1668d2c487d304a7f49b41b4429
yw.hao <yw.hao@trip.com>
2024-03-10 21:16:48 +0800

    feat: add reply and sports app

--- basic-android-kotlin-compose-training-sports/app/src/main/java/com/example/sports/MainActivity.kt
+++ basic-android-kotlin-compose-training-sports/app/src/main/java/com/example/sports/MainActivity.kt
@@ -13,63 +13,34 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.example.reply
+
+package com.example.sports
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.Surface
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
-import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.reply.ui.ReplyApp
-import com.example.reply.ui.theme.ReplyTheme
+import com.example.sports.ui.SportsApp
+import com.example.sports.ui.theme.SportsTheme
 
+/**
+ * Activity for Sports app
+ */
 class MainActivity : ComponentActivity() {
-
     @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
-
         setContent {
-            ReplyTheme {
+            SportsTheme {
                 Surface {
                     val windowSize = calculateWindowSizeClass(activity = this)
-                    ReplyApp(windowSize = windowSize.widthSizeClass)
+                    SportsApp(windowSize = windowSize.widthSizeClass)
                 }
             }
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun ReplyAppCompactPreview() {
-    ReplyTheme {
-        Surface {
-            ReplyApp(windowSize = WindowWidthSizeClass.Compact)
-        }
-    }
-}
-
-@Preview(showBackground = true, widthDp = 700)
-@Composable
-fun ReplyAppMediumPreview() {
-    ReplyTheme {
-        Surface {
-            ReplyApp(windowSize = WindowWidthSizeClass.Medium)
-        }
-    }
-}
-
-@Preview(showBackground = true, widthDp = 1000)
-@Composable
-fun ReplyExpandPreview() {
-    ReplyTheme {
-        Surface {
-            ReplyApp(windowSize = WindowWidthSizeClass.Expanded)
         }
     }
 }
