b83a773fcfbad232d3866afa698ad5ff6c35f94a
yw.hao <yw.hao@trip.com>
2024-04-22 01:04:40 +0800

    feat: java view add navigation graph

--- xml-and-compose-view-samples/java_view_other/src/main/java/com/yiwen/java_view_other/NavigationFragmentActivity.java
+++ xml-and-compose-view-samples/java_view_other/src/main/java/com/yiwen/java_view_other/NavigationFragmentActivity.java
@@ -7,18 +7,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.yiwen.java_view_other.databinding.ActivityNavigationFragmentBinding;
 
 public class NavigationFragmentActivity extends AppCompatActivity {
+    ActivityNavigationFragmentBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+        binding = ActivityNavigationFragmentBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.fragment_host_navigation);
+
+        NavController navController = navHostFragment.getNavController();
+
+
     }
 }
