7f7db1748f842d9d036f9049e95e79280acf28e7
yw.hao <yiwenlemo@gmail.com>
2024-05-06 21:32:31 +0800

    feat: add code

--- xml-and-compose-view-samples/app/src/main/java/com/example/javaviewtest/ReactNativeFragmentActivity.java
+++ xml-and-compose-view-samples/app/src/main/java/com/example/javaviewtest/ReactNativeFragmentActivity.java
@@ -10,11 +10,17 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 
+import com.facebook.react.BuildConfig;
 import com.facebook.react.ReactFragment;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
 import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
+import com.facebook.react.shell.MainReactPackage;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class ReactNativeFragmentActivity extends AppCompatActivity implements DefaultHardwareBackBtnHandler {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,11 +34,7 @@ public class ReactNativeFragmentActivity extends AppCompatActivity implements De
 
         Button button = findViewById(R.id.button_rn);
         button.setOnClickListener(v -> {
-            Fragment reactNativeFragment = new ReactFragment.Builder()
-                    .setComponentName("HelloWorld")
-                    .setLaunchOptions(getLaunchOptions("test"))
-                    .setFabricEnabled(false)
-                    .build();
+            Fragment reactNativeFragment = new CustomReactFragment();
 
             getSupportFragmentManager()
                     .beginTransaction()
--- xml-and-compose-view-samples/react-native-container/src/main/java/io/github/haoyiwen/react_native_container/ReactNativeActivity.java
+++ xml-and-compose-view-samples/react-native-container/src/main/java/io/github/haoyiwen/react_native_container/ReactNativeActivity.java
@@ -1,41 +1,22 @@
 package io.github.haoyiwen.react_native_container;
 
-import android.app.Activity;
-import android.content.ComponentName;
-import android.content.Intent;
 import android.content.res.AssetManager;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.Settings;
-import android.view.KeyEvent;
 
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 
-import com.facebook.hermes.intl.DateTimeFormat;
-import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
 import com.facebook.react.BuildConfig;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactInstanceManager;
-import com.facebook.react.ReactInstanceManagerBuilder;
+import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.ReactRootView;
-import com.facebook.react.common.LifecycleState;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
-import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
 import com.facebook.react.shell.MainReactPackage;
-import com.facebook.soloader.SoLoader;
-import com.reactnativecommunity.webview.RNCWebViewPackage;
-import com.swmansion.rnscreens.RNScreensPackage;
-import com.th3rdwave.safeareacontext.SafeAreaContextPackage;
 //import com.swmansion.rnscreens.RNScreensPackage;
 
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeFormatterBuilder;
 import java.util.Arrays;
+import java.util.List;
 
 
 public class ReactNativeActivity extends ReactActivity {
@@ -52,6 +33,7 @@ public class ReactNativeActivity extends ReactActivity {
         return "MyReactNativeApp";
     }
 
+
     @Override
     protected ReactActivityDelegate createReactActivityDelegate() {
         return new DefaultReactActivityDelegate(this, getMainComponentName(), false) {
@@ -62,11 +44,13 @@ public class ReactNativeActivity extends ReactActivity {
                 initialProperties.putString("initParam", "value");
                 return initialProperties;
             }
-        };
-    }
 
-    public ReactInstanceManager getmReactInstanceManager() {
-        return mReactInstanceManager;
+            @Override
+            protected ReactNativeHost getReactNativeHost() {
+
+                return super.getReactNativeHost();
+            }
+        };
     }
 
     @Override
