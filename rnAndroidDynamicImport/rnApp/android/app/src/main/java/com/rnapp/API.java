20bd2278982fd6ec8bc694ed699b033f2d054b3a
yw.hao <yw.hao@trip.com>
2024-05-06 13:57:40 +0800

    feat: 动态引入rn bundle测试

---
+++ rnAndroidDynamicImport/rnApp/android/app/src/main/java/com/rnapp/API.java
@@ -0,0 +1,7 @@
+package com.rnapp;
+
+public class API {
+    public static String IP = "http://10.67.51.61:3000/";
+    public static String MODULES = IP + "modules";
+    public static String DOWNLOAD = IP + "download/";
+}
