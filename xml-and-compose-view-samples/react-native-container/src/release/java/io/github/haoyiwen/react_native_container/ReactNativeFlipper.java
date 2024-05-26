5abe5ff2b7f9cf52068dc77f97fb05892abca7e8
yw.hao <yiwenlemo@gmail.com>
2024-05-26 13:42:39 +0800

    feat: add flipper plugin
    添加flipper插件，捕获网络请求

---
+++ xml-and-compose-view-samples/react-native-container/src/release/java/io/github/haoyiwen/react_native_container/ReactNativeFlipper.java
@@ -0,0 +1,9 @@
+package io.github.haoyiwen.react_native_container;
+import android.content.Context;
+import com.facebook.react.ReactInstanceManager;
+
+public class ReactNativeFlipper {
+    public static void initializeFlipper(Context context, ReactInstanceManager reactInstanceManager) {
+
+    }
+}
