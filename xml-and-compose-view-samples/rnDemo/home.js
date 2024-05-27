53c9c62e45559e5c41b30a1d9e4919714260cac8
yw.hao <yiwenlemo@gmail.com>
2024-05-28 03:08:55 +0800

    feat: add rn multi instance

--- xml-and-compose-view-samples/rnDemo/home.js
+++ xml-and-compose-view-samples/rnDemo/home.js
@@ -1,10 +1,21 @@
 import React from "react";
-import { AppRegistry, View, Text } from "react-native";
+import { AppRegistry, View, Text, Button } from "react-native";
+import { NativeModules } from "react-native";
 
 const App = () => {
   return (
     <View style={{ flex: 1 }}>
       <Text>rn base page</Text>
+      <Button
+        title="Click me"
+        onPress={() => {
+          NativeModules.IntentModule.startActivityFromJS(
+            "MyReactNativeApp",
+            "index.android.bundle",
+            true
+          );
+        }}
+      />
     </View>
   );
 };
