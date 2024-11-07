4eac8f0d70df252cf32b65c1b1dce35724bd354a
yw.hao <yw.hao@trip.com>
2024-11-07 20:44:51 +0800

    feat: 升级到android35

--- xml-and-compose-view-samples/build.gradle.kts
+++ xml-and-compose-view-samples/build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.library") version "8.4.0" apply false
+    id("com.android.library") version "8.7.0" apply false
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
     alias(libs.plugins.googleAndroidLibrariesMapsplatformSecretsGradlePlugin) apply false
@@ -13,10 +13,10 @@ buildscript {
 }
 
 // 使用 extra 定义扩展属性
-extra["buildToolsVersion"] = "30.0.3"
+extra["buildToolsVersion"] = "34.0.0"
 extra["compileSdkVersion"] = 35
 extra["minSdkVersion"] = 28
 extra["targetSdkVersion"] = 35
-extra["versionCode"] = 1
+extra["versionCode"] = 2
 extra["versionName"] = "1.0"
 
